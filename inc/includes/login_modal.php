b8d2d9c0a24b4374cf131a91c3615996e3a1aa60
Radosław Pochopień <radek.p.p@wp.pl>
2021-12-19 18:21:43 +0100

    init

---
+++ inc/includes/login_modal.php
@@ -0,0 +1,31 @@
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Logowanie</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- LOGIN FORM-->
+                <form>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e-mail">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="hasło">
+                    </div>
+                    <button type="submit" class="btn btn-primary" style="margin: auto; display:block; padding-left: 20px; padding-right:20px; ">Zaloguj</button>
+                </form>
+
+                <hr />
+                <button type="button" class="btn btn-dark w-100 mb-2"><i class="fab fa-facebook me-2"></i> Zaloguj korzystając z Facebook'a</button> <br />
+                <button type="button" class="btn btn-dark w-100 mb-2"><i class="fab fa-google me-2"></i> Zaloguj korzystając z Google'a</button> <br />
+                <button type="button" class="btn btn-dark w-100 mb-2"><i class="fab fa-twitter me-2"></i> Zaloguj korzystając z Twtitter'a</button>
+
+
+                <!-- END LOGIN FORM -->
+            </div>
+        </div>
+    </div>
+</div>
