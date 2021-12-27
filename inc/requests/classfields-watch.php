e2386dfb48e21700c34f9db2791ed161d53715d2
Radosław Pochopień <radek.p.p@wp.pl>
2021-12-27 13:36:51 +0100

    Created watch

---
+++ inc/requests/classfields-watch.php
@@ -0,0 +1,23 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/config/database.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/config/config.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/inc/functions/sfm.php");
+
+$classfields = $database->select("classfield",
+ ["[>]user" => ["user_id" => "userid"],
+  "[>]classfield_photo" => ["id" => "classfield_id", "AND" => [
+                "main=" => 1
+            ]
+        ]
+
+ ],
+ ["classfield.id", "classfield.title", "classfield.created_at", "classfield.location", "classfield.cost","classfield_photo.photo_hash",
+]);
+foreach($classfields as $key => $value){
+
+    $classfields[$key]['cost'] = cost_formatter($value['cost']);
+    $classfields[$key]['link'] = '/post/'.clean($value['title'])."-".$value['id'];
+}
+
+
+echo json_encode($classfields);
