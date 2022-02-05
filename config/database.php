59189070142af43a32a533e4e1003978c3af2547
Radosław Pochopień <radek.p.p@wp.pl>
2022-02-05 12:07:07 +0100

    prod database

--- config/database.php
+++ config/database.php
@@ -1,19 +1,19 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT']. '/assets/libs/medoo/Medoo.php');
 // Using Medoo namespace.
-$db_login = "root";
-$db_password = "Radekp123!!!";
+$db_login = "schron_prod";
+$db_password = "g3qpGNHO74HwJFzS";
 $db_host = "167.86.120.16";
-$db_database = "schron";
+$db_database = "schron_production";
 use Medoo\Medoo;
 
 $database = new Medoo([
     // [required]
     'type' => 'mysql',
-    'host' => '167.86.120.16',
-    'database' => 'schron',
-    'username' => 'root',
-    'password' => 'Radekp123!!!',
+    'host' => $db_host,
+    'database' => $db_database,
+    'username' => $db_login,
+    'password' => $db_password,
     'charset' => 'utf8',
  
 
