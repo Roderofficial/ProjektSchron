eb5deb670a4bbc832e481f4650fd000e249bc7ee
Radosław Pochopień <radek.p.p@wp.pl>
2022-02-06 17:57:42 +0100

    Poprawka orderu kategorii

--- inc/requests/categories.php
+++ inc/requests/categories.php
@@ -1,7 +1,11 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"].'/config/database.php');
 
-$categories = $database->select("classfield_category", ["ctid(id)", "category_title(text)", "category_icon(icon)"]);
+$categories = $database->select("classfield_category", ["ctid(id)", "category_title(text)", "category_icon(icon)"], 
+["ORDER" => [
+    "orderby" => "DESC"
+]]
+);
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($categories);
 
