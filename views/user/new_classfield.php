d8182b2ee47a63cee7c23e33234e5b5c20f4e876
Radosław Pochopień <radek.p.p@wp.pl>
2022-02-10 09:31:46 +0100

    Zmiana wymaganych pól, czas ogłoszenia

--- views/user/new_classfield.php
+++ views/user/new_classfield.php
@@ -28,11 +28,12 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title" id="labelnewclassfieldtitle">Nowe ogłoszenie</h5>
+
                 <form method="POST" id="classfield">
 
                     <!-- OBRAZY -->
                     <div class="obrazy mb-3">
-                        <h6 class="card-subtitle mb-2 text-muted ">Zdjęcia</h6>
+                        <h6 class="card-subtitle mb-2 text-muted ">Zdjęcia<span style="color:red;"><sup>*</sup></span></h6>
                         <div id="drag-drop-area"></div>
                         <div class="hidden-images" style="display:none"></div>
                         <small class="text-muted">Pierwsze zdjęcie będzie zdjęciem głównym.</small>
@@ -43,11 +44,11 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
 
                         <!-- DETAIL EDITOR -->
                         <div class="mb-3">
-                            <label>Tytuł ogłoszenia</label>
+                            <label>Tytuł ogłoszenia<span style="color:red;"><sup>*</sup></span></label>
                             <input class="form-control form-control-lg" type="text" placeholder="tytuł ogłoszenia" name="title" aria-label=".form-control-lg example" min="1" max="100" required>
                         </div>
                         <div class="mb-3">
-                            <label>Treść ogłoszenia</label>
+                            <label>Treść ogłoszenia<span style="color:red;"><sup>*</sup></span></label>
                             <div id="editor">
                                 <textarea id="desceditor" minlength="100" maxlength="6000"></textarea>
                                 <textarea name="description" id="description" style="display:none;"></textarea>
@@ -63,14 +64,14 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
                         <div class="catcost mb-3">
                             <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
                                 <div class="col-lg-6 col-12">
-                                    <label>Kategoria</label>
+                                    <label>Kategoria<span style="color:red;"><sup>*</sup></span></label>
                                     <select class="category-select form-control" name="category" id="new-classfield-category-picker" style="width:100%;" required>
                                     </select>
                                 </div>
                                 <div class="col-lg-6 col-12">
-                                    <label>Cena</label>
+                                    <label>Cena<span style="color:red;"><sup>*</sup></span></label>
                                     <div class="input-group mb-3">
-                                        <input type="number" class="form-control" name="cost" placeholder="0 zł" aria-describedby="basic-addon2" min="0" step="1" required>
+                                        <input type="number" class="form-control" name="cost" placeholder="0 zł" aria-describedby="basic-addon2" min="0" step="1" val="0" required>
                                         <span class="input-group-text" id="basic-addon2">zł</span>
                                     </div>
                                 </div>
@@ -84,11 +85,11 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
                             <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
                                 <div class="col-lg-6 col-12">
                                     <label>Adres e-mail</label>
-                                    <input type="email" class="form-control" name="email" placeholder="name@example.com" value="<?= $placeholder["email_public"] ?>" required>
+                                    <input type="email" class="form-control" name="email" placeholder="name@example.com" value="<?= $placeholder["email_public"] ?>">
 
                                 </div>
                                 <div class="col-lg-6 col-12">
-                                    <label>Telefon</label>
+                                    <label>Telefon<span style="color:red;"><sup>*</sup></span></label>
                                     <input type="tel" class="form-control" id="phone" name="phone" placeholder="+48 123 456 789" pattern="(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)" value="<?= $placeholder["phone_public"] ?>" required>
 
                                 </div>
@@ -101,7 +102,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
                         <div class="mb-3 lokalizacja">
                             <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|} location">
                                 <div class="col-lg-6 col-12">
-                                    <label>Lokalizacja</label>
+                                    <label>Lokalizacja<span style="color:red;"><sup>*</sup></span></label>
                                     <select class="form-control citypicker" id="citypicker" name="osm_id" placeholder="Szukaj..." style="width:100%;">
                                         <option value="" disabled selected>Szukaj...</option>
                                     </select>
@@ -110,6 +111,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
                             </div>
 
                         </div>
+                        <small>Pola oznaczone <span style="color:red;">*</span> są wymagane.</small>
                     </div>
 
                     <!-- PRZESYŁANIE FORMULARZA -->
@@ -117,6 +119,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/inc/new_classfield/controler.php');
                         <div style="text-align:center;">
                             <div class="g-recaptcha mb-3" style="display:inline-block" data-sitekey="6Ldl6d8dAAAAANuCA-InONqkE0EIWnuoMRDyIqGb"></div>
                         </div>
+
                         <button class="btn btn-primary" type="submit" id="formclassfieldsubmit">Dodaj ogłoszenie</button>
 
                     </div>
