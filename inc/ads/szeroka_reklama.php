12da72a51680e93e941096c2696abc387c721539
Radoslaw Pochopień <admin@nanodc.xyz>
2022-02-28 10:29:08 +0100

    try align center

--- inc/ads/szeroka_reklama.php
+++ inc/ads/szeroka_reklama.php
@@ -1,1 +1,1 @@
-<div class="containter mb-3 ms-3 mr-auto ml-auto"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3633819687172356" data-ad-slot="2527039530" data-ad-format="auto" data-full-width-responsive="true"></ins></div>
+<div class="containter mb-3 mt-3" align="center"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3633819687172356" data-ad-slot="2527039530" data-ad-format="auto" data-full-width-responsive="true"></ins></div>
