0d310d7cc9f2ffc92cb1dc8e9dce05bf182265bf
Radoslaw Pochopień <admin@nanodc.xyz>
2022-02-28 10:16:52 +0100

    ads,margin fix

--- inc/ads/szeroka_reklama.php
+++ inc/ads/szeroka_reklama.php
@@ -1,1 +1,1 @@
-<div class="containter mb-3"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3633819687172356" data-ad-slot="2527039530" data-ad-format="auto" data-full-width-responsive="true" data-adtest="on" data-ad-test="on" google_adtest=on></ins></div>
+<div class="containter mb-3 ms-3 mr-auto ml-auto"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3633819687172356" data-ad-slot="2527039530" data-ad-format="auto" data-full-width-responsive="true"></ins></div>
