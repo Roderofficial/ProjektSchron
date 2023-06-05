129be769dec16a0286268cd987c834f324de36a9
Radosław Pochopień <radek.p.p@wp.pl>
2023-06-05 10:00:41 +0200

    config clean

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+$photo_source = "/assets/images/test-dogs/";
+$banner_location = "/assets/images/backgrounds/";
+$avatar_locations = "/assets/images/avatars/";
+$default_banner= "/assets/images/avatars/default.jpg";
+$default_avatar = "/assets/images/backgrounds/default.jpg";
+$images_classfield_location = "/assets/images/classfields/";
+$grechapta_secret = '';
+$filetype_allowed = array("image/jpeg", "image/png", "image/jpg");
+
+$config["mail"]["host"] = "";
+$config["mail"]["username"] = "";
+$config["mail"]["password"] = "";
+?>
